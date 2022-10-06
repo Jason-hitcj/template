688f4f936166331bbaedd85f207dc7cc43822c02
jason <1953310378@qq.com>
2022-10-06 21:33:35 +0800

    实现了词法分析器

--- src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
+++ src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
@@ -1,16 +1,21 @@
 package cn.edu.hitsz.compiler.lexer;
 
-import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.StreamSupport;
+
+
 
 /**
  * TODO: 实验一: 实现词法分析
  * <br>
  * 你可能需要参考的框架代码如下:
  *
+ * @author jason
  * @see Token 词法单元的实现
  * @see TokenKind 词法单元类型的实现
  */
@@ -20,7 +25,10 @@ public class LexicalAnalyzer {
     public LexicalAnalyzer(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
     }
-
+    private List<String> buf = new ArrayList<>();
+    private List<Character> charlist = new LinkedList<>();
+    private int p = 0;
+    private List<Token> tokenLab = new LinkedList<>();
 
     /**
      * 从给予的路径中读取并加载文件内容
@@ -31,7 +39,18 @@ public class LexicalAnalyzer {
         // TODO: 词法分析前的缓冲区实现
         // 可自由实现各类缓冲区
         // 或直接采用完整读入方法
-        throw new NotImplementedException();
+
+        //按行读取文件
+        this.buf = FileUtils.readLines(path);
+        int i;
+        //按照字符存入charlist，方便自动机检查
+        for (String s : buf) {
+            for (i = 0; i < s.length(); i++) {
+                charlist.add(s.charAt(i));
+            }
+        }
+        //添加'$'作为结束标志
+        charlist.add('$');
     }
 
     /**
@@ -40,7 +59,81 @@ public class LexicalAnalyzer {
      */
     public void run() {
         // TODO: 自动机实现的词法分析过程
-        throw new NotImplementedException();
+        while (true){
+            //ch存储字符，作为自动机跳转判断
+            char ch;
+            //p作为链表末指针
+            ch = charlist.get(p++);
+
+            //跳过空格
+            while(ch == ' '){
+                ch = charlist.get(p++);
+            }
+
+            //字母判断
+            if (Character.isLowerCase(ch) || Character.isUpperCase(ch)){
+                //临时存储
+                StringBuilder string = new StringBuilder();
+                while(Character.isLetterOrDigit(ch)){
+                    //采用拼接方式将字符组成字符串
+                    string.append(ch);
+                    ch = charlist.get(p++);
+                }
+                //尾指针回退
+                p--;
+                //判断是否为标识符
+                if (TokenKind.isAllowed(string.toString())){
+                    tokenLab.add(Token.simple(string.toString()));
+                }
+                else{
+                    tokenLab.add(Token.normal("id", string.toString()));
+                }
+
+
+            }
+            //数字判断
+            else if (Character.isDigit(ch)){
+                //同上
+                StringBuilder string = new StringBuilder();
+                while (Character.isDigit(ch)){
+                    string.append(ch);
+                    ch = charlist.get(p++);
+                }
+                //尾指针回退
+                p--;
+                tokenLab.add(Token.normal("IntConst", string.toString()));
+
+            }
+            //结束判断，跳出while循环
+            else if (ch == '$'){
+                tokenLab.add(Token.eof());
+                break;
+            }
+            //其他字符
+            else{
+                switch(ch){
+                    case '+':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    case '-':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    case '*':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    case '/':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    case '=':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    case ';':
+                        tokenLab.add(Token.simple("Semicolon"));break;
+                    case '(':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    case ')':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    case ',':
+                        tokenLab.add(Token.simple(String.valueOf(ch)));break;
+                    default:
+                }
+            }
+        }
     }
 
     /**
@@ -53,7 +146,8 @@ public class LexicalAnalyzer {
         // 词法分析过程可以使用 Stream 或 Iterator 实现按需分析
         // 亦可以直接分析完整个文件
         // 总之实现过程能转化为一列表即可
-        throw new NotImplementedException();
+        return tokenLab;
+
     }
 
     public void dumpTokens(String path) {
